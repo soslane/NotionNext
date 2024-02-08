4f77ee964a6a023fb689a85020db539e3dc9d501
soslane <149466045+soslane@users.noreply.github.com>
2024-02-08 14:41:33 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
-  HEXO_HOME_BANNER_GREETINGS: [''], // 首页大图标语文字
+  HEXO_HOME_BANNER_GREETINGS: ['Smile to the fucking life'], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
