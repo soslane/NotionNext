1a01c80d8be5908ad4797396b6055f664dfcce41
soslane <149466045+soslane@users.noreply.github.com>
2024-02-12 22:53:05 +0800

    Update config.js

--- themes/hexo/config.js
+++ themes/hexo/config.js
@@ -1,7 +1,7 @@
 const CONFIG = {
   HEXO_HOME_BANNER_ENABLE: true,
   // 3.14.1以后的版本中，欢迎语在blog.config.js中配置，用英文逗号','隔开多个。
-  HEXO_HOME_BANNER_GREETINGS: ['Smile to the fucking life'], // 首页大图标语文字
+  HEXO_HOME_BANNER_GREETINGS: [''], // 首页大图标语文字
 
   HEXO_HOME_NAV_BUTTONS: false, // 首页是否显示分类大图标按钮
   // 已知未修复bug, 在移动端开启true后会加载不出图片； 暂时建议设置为false。
@@ -17,7 +17,7 @@ const CONFIG = {
   HEXO_MENU_SEARCH: true, // 显示搜索
 
   HEXO_POST_LIST_COVER: true, // 列表显示文章封面
-  HEXO_POST_LIST_COVER_HOVER_ENLARGE: false, // 列表鼠标悬停放大
+  HEXO_POST_LIST_COVER_HOVER_ENLARGE: true, // 列表鼠标悬停放大
 
   HEXO_POST_LIST_COVER_DEFAULT: true, // 封面为空时用站点背景做默认封面
   HEXO_POST_LIST_SUMMARY: true, // 文章摘要
