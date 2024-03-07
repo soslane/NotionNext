b82203f0964d5ec926ce8fce23e5906af114110a
soslane <149466045+soslane@users.noreply.github.com>
2024-03-07 21:02:01 +0800

    Update Logo.js

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -4,8 +4,10 @@ import Link from 'next/link'
 const Logo = props => {
   return (
     <Link href='/' passHref legacyBehavior>
-      <div className='flex flex-col justify-center items-center cursor-pointer space-y-3'>
-        <div className='font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'> {siteConfig('TITLE') }</div>
+      <div className='flex justify-center items-center cursor-pointer space-y-3'>
+        {/* eslint-disable-next-line @next/next/no-img-element */}
+        <img src='https://pic.imgdb.cn/item/65bc7b2e871b83018a758216.png' className='w-auto h-10'/>
+        <div className='invisible font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'> {siteConfig('TITLE') }</div>
       </div>
     </Link>
   )
