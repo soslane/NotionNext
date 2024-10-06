05e3247d5a65a7a7d570a53fd505b602520a082c
soslane <149466045+soslane@users.noreply.github.com>
2024-10-06 14:48:03 +0800

    Update Logo.js

--- themes/hexo/components/Logo.js
+++ themes/hexo/components/Logo.js
@@ -6,7 +6,7 @@ const Logo = props => {
     <Link href='/' passHref legacyBehavior>
       <div className='flex justify-center items-center cursor-pointer space-y-3'>
         {/* eslint-disable-next-line @next/next/no-img-element */}
-        <img src='https://img.myla.eu.org/api/file/ad9308e6b3653ffe18d1a.png' className='w-auto h-10'/>
+        <img src='https://pic.myla.eu.org/file/1728197253801_ormed-e1727272317479.png' className='w-auto h-10'/>
         <div className='invisible font-medium text-lg p-1.5 rounded dark:border-white dark:text-white menu-link transform duration-200'> {siteConfig('TITLE') }</div>
       </div>
     </Link>
