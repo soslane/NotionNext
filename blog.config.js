a4197529a5e34876f5909d7cc6784e4e05e2a720
soslane <149466045+soslane@users.noreply.github.com>
2024-11-27 08:14:40 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -52,14 +52,13 @@ const BLOG = {
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
     // 'https://fonts.googleapis.com/css?family=Bitter&display=swap',
-    'https://fonts.googleapis.com/css2?family=Lato:wght@400;700&display=swap',
-    'https://fonts.googleapis.com/css2?family=Noto+Sans:wght@400;700&display=swap',
+    'https://fonts.googleapis.com/css?family=Bitter&display=swap',
+    'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap',
+    'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
-    '"Lato"',
-    '"Noto Sans SC"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
@@ -79,7 +78,6 @@ const BLOG = {
   // 衬线字体 例如'"LXGW WenKai"'
   FONT_SERIF: [
     // '"LXGW WenKai"',
-    '"Noto Sans SC"',
     'Bitter',
     '"Noto Serif SC"',
     'SimSun',
