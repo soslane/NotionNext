1b1c2044da038acb4c817b4b25dd1d58aac667b7
soslane <149466045+soslane@users.noreply.github.com>
2024-11-27 10:20:53 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -52,15 +52,13 @@ const BLOG = {
   FONT_URL: [
     // 'https://npm.elemecdn.com/lxgw-wenkai-webfont@1.6.0/style.css',
     // 'https://fonts.googleapis.com/css?family=Bitter&display=swap',
-    'https://fonts.googleapis.com/css2?family=Roboto:ital,wght@300&display=swap'，
     'https://fonts.googleapis.com/css2?family=Noto+Sans+SC:wght@300&display=swap'，
     'https://fonts.googleapis.com/css2?family=Noto+Serif+SC:wght@300&display=swap'
   ],
   // 无衬线字体 例如'"LXGW WenKai"'
   FONT_SANS: [
     // '"LXGW WenKai"',
-    '"Roboto"',
-    '"Noto Sans SC"',
+    '"Microsoft Yahei"',
     '"PingFang SC"',
     '-apple-system',
     'BlinkMacSystemFont',
