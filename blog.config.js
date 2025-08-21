99d4ae0760cef4e6a614646b6d9dc4a24e60efb5
soslane <149466045+soslane@users.noreply.github.com>
2025-08-22 00:34:25 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -21,7 +21,7 @@ const BLOG = {
   BIO: process.env.NEXT_PUBLIC_BIO || 'Cloud Computing Team @ Gweek', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.myla.eu.org', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Cloud computing, Blog', // 网站关键词 英文逗号隔开
-
+  
   // 社交链接，不需要可留空白，例如 CONTACT_WEIBO:''
   CONTACT_EMAIL: process.env.NEXT_PUBLIC_CONTACT_EMAIL || '', // 邮箱地址 例如mail@tangly1024.com
   CONTACT_WEIBO: process.env.NEXT_PUBLIC_CONTACT_WEIBO || 'https://weibo.com/soslane', // 你的微博个人主页
